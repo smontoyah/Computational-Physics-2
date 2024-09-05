9b86014f005ddcea95ae781f22bee3cf2a5a2eca
Sebastian Montoya <smontoyah99@gmail.com>
2024-09-05 04:34:32 -0500

    first commit

---
+++ clase1.c
@@ -0,0 +1,17 @@
+//this is a comment
+
+#include <iostream> // permite llamar cout y cin
+
+
+int main() { //int -> funcion entera con retorno entero
+    // Imprimir el mensaje en la consola
+    std::cout << "Hola mundo" << std::endl;
+   
+    
+    // Retornar 0 para indicar que el programa terminó exitosamente
+    return 0;
+}
+
+//compilar en terminal
+//g++ -o myexecutable clase1.c
+// ./myexecutable
