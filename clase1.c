83633c8b53ec01777aa422b2f7789eee8ec830c9
Sebastian Montoya <smontoyah99@gmail.com>
2024-09-05 04:42:32 -0500

    uso de cout

--- clase1.c
+++ clase1.c
@@ -1,17 +1,26 @@
-//this is a comment
+// Este es un comentario
 
-#include <iostream> // permite llamar cout y cin
+#include <iostream> // Permite usar cout y cin
+#include <string>   // Necesario para usar std::string
 
-
-int main() { //int -> funcion entera con retorno entero
+int main() { // int -> función entera con retorno entero
     // Imprimir el mensaje en la consola
     std::cout << "Hola mundo" << std::endl;
    
-    
+    std::string nombre; // Se necesita especificar el tipo de dato std::string
+    float edad;         // Se necesita especificar el tipo de dato float
+
+    std::cout << "Digite el nombre: "; // Se usa << en lugar de >>
+    std::getline(std::cin, nombre);    // std::getline es mejor para leer nombres con espacios
+
+    std::cout << "Digite edad: "; // Se usa << en lugar de >>
+    std::cin >> edad;
+
+    std::cout << "Nombre: " << nombre << ", Edad: " << edad << std::endl;
     // Retornar 0 para indicar que el programa terminó exitosamente
     return 0;
 }
 
-//compilar en terminal
-//g++ -o myexecutable clase1.c
+// Compilar en terminal
+// g++ -o myexecutable programa.cpp
 // ./myexecutable
